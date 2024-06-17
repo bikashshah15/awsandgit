f16d0527cca4a2aec26d9c968f24710aa2b339ad
Bikash Shah <bikashshah15@gmail.com>
2024-06-17 13:48:55 -0500

    Added

---
+++ src/main/java/com/awsandgit/AwSandGitApplication.java
@@ -0,0 +1,13 @@
+package com.awsandgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwSandGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwSandGitApplication.class, args);
+    }
+
+}
